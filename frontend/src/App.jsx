01514b4f7eb3e7dc17af046d1bd2dfc6bf5e3b5f
jannat310 <jannat14101993@gmail.com>
2026-06-22 12:57:57 +0500

    fix mongodb uri, upstash redis, homepage import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/homepage' 
-import NoteDetailPage from './pages/NoteDetailPage'
-import CreatePage from './pages/createPage'
-import toast  from 'react-hot-toast'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Homepage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import CreatePage from "./pages/createPage";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
     <div className="relative h-full w-full">
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
-      
+
       <Routes>
-        <Route path = "/" element={<HomePage />} />
-        <Route path = "/create" element={<CreatePage />} />
-        <Route path = "/note/:id" element={<NoteDetailPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
       </Routes>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
