5c0ea643b3b6dc670e95da8684a39725a25f0983
jannat310 <jannat14101993@gmail.com>
2026-06-23 15:21:12 +0500

    change route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,52 +1,47 @@
 import express from "express";
-import notesRoutes from "./routes/notesRoutes.js";
-import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
-import rateLimiter from "./middleware/rateLimiter.js";  
 import cors from "cors";
+import dotenv from "dotenv";
 import path from "path";
 
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
-
-
 const app = express();
-const PORT=process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-if(process.env.NODE_ENV !== "production"){
-  app.use(cors({
-  origin: "http://localhost:5173"
-}));
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
 }
-
-app.use(express.json()); // Middleware to parse JSON request bodies: req.body
- 
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
-// simple custom middlewareto log incoming req
+// our simple custom middleware
 // app.use((req, res, next) => {
-//   console.log(`req method is ${req.method} and req url is ${req.url}`);
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
 //   next();
-// })
+// });
 
 app.use("/api/notes", notesRoutes);
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("/{*path}", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
-});
+  app.get("/{*path}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
+
 connectDB().then(() => {
-app.listen(PORT, () => {
-  console.log("Server is running now on port 5001", PORT);
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
-});
-
-
-
-
-//  mongodb+srv://maryam2015usama_db_user:1lrRfEzWkX92SEO0@cluster0.asdk2a2.mongodb.net/?appName=Cluster0
